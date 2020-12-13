2400afd4af5b65e28dcda7878dcfce9d1465721f
Mikhaela07 <mikimouse9997@gmail.com>
2020-12-14 00:48:27 +0200

    maybe)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  resources :actors
-  resources :premieres
-  resources :teams
+
   get 'page/index'
   root 'page#index'
   get '/actors', to: 'actors#index', as: 'actor'
   get '/films', to: 'films#index', as: 'films'
 
-  resources :films
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
